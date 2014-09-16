f41848fc1024e5d753060a9d7100c562df4ec522
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-09-16 18:44:43 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+kursitet_cas_mapper
+===============
+
+CAS attribute mapper for kursitet-platform/edX integration.
---
+++ kursitet_cas_mapper/__init__.py
@@ -0,0 +1,51 @@
+"""
+Beyond the original function of CAS attribute mapper, this should also manage other objects...
+""" 
+
+VERSION = "0.0.1"
+
+# Note: In production this is supposed to be https.
+# I'm not even sure how to test that correctly without it...
+CAS_URI = 'https://kursitet.ru/cas'
+
+NSMAP = {'cas': CAS_URI}
+CAS = '{%s}' % CAS_URI
+
+from django.core.exceptions import ObjectDoesNotExist
+from student.models import UserProfile
+
+def populate_user(user, authentication_response):
+    if authentication_response.find(CAS + 'authenticationSuccess/'  + CAS + 'attributes'  , namespaces=NSMAP) is not None:
+        attr = authentication_response.find(CAS + 'authenticationSuccess/'  + CAS + 'attributes'  , namespaces=NSMAP)
+
+        if attr.find(CAS + 'is_staff', NSMAP) is not None:
+            user.is_staff = attr.find(CAS + 'is_staff', NSMAP).text.upper() == 'TRUE'
+
+        if attr.find(CAS + 'is_superuser', NSMAP) is not None:
+            user.is_superuser = attr.find(CAS + 'is_superuser', NSMAP).text.upper() == 'TRUE'
+
+        if attr.find(CAS + 'givenName', NSMAP) is not None:
+            user.first_name = attr.find(CAS + 'givenName', NSMAP).text
+
+        if attr.find(CAS + 'sn', NSMAP) is not None:
+            user.last_name = attr.find(CAS + 'sn', NSMAP).text
+
+        if attr.find(CAS + 'email', NSMAP) is not None:
+            user.email = attr.find(CAS + 'email', NSMAP).text
+        
+        # Here we handle things that go into UserProfile instead.
+        
+        try:
+			user_profile = UserProfile.objects.get(user=user)
+		except ObjectDoesNotExist:
+            user_profile = UserProfile(user=user)
+            
+	    # There should be more variables, but let's settle on the actual model first.
+        if attr.find(CAS + 'fullName', NSMAP) is not None:
+			user_profile.name = attr.find(CAS + 'fullName', NSMAP).text
+	
+	    # Profile is always getting saved, just like the user,
+	    # but the user is getting saved by django_cas.
+        user_profile.save()
+        
+    pass
