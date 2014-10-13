16ac49a56605eb49f273471ea3bb3ef9a77267dd
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-10-13 05:21:01 +0400

    Argh.

--- kursitet_cas_mapper/__init__.py
+++ kursitet_cas_mapper/__init__.py
@@ -76,6 +76,6 @@ def populate_user(user, authentication_response):
                     # and CourseEnrollment objects do no checking of their own.
                     # Being enrolled in a deleted course should not be an issue though...
                     org, course, run = course.split('/')
-                    CourseEnrollment.enroll(user,CourseLocator.from_string(org=org,course=course,run=run))
+                    CourseEnrollment.enroll(user,CourseLocator(org=org,course=course,run=run))
         
     pass
