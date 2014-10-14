c89339cd44b1ad4544649d879f33a1f94ecfdc85
Eugene Medvedev <rn3aoh.g@gmail.com>
2014-10-14 16:27:41 +0400

    Using accumulated knowledge from the subproduction branch.

--- kursitet_cas_mapper/__init__.py
+++ kursitet_cas_mapper/__init__.py
@@ -1,6 +1,6 @@
 """
 Beyond the original function of CAS attribute mapper, this should also manage other objects...
-""" 
+"""
 
 CAS_URI = 'http://www.yale.edu/tp/cas'
 
@@ -10,53 +10,65 @@ CAS = '{%s}' % CAS_URI
 import json
 
 def populate_user(user, authentication_response):
-    if authentication_response.find(CAS + 'authenticationSuccess/'  + CAS + 'attributes'  , namespaces=NSMAP) is not None:
-        attr = authentication_response.find(CAS + 'authenticationSuccess/'  + CAS + 'attributes'  , namespaces=NSMAP)
 
-        if attr.find(CAS + 'is_staff', NSMAP) is not None:
-            user.is_staff = attr.find(CAS + 'is_staff', NSMAP).text.upper() == 'TRUE'
+    attr = authentication_response.find(CAS + 'authenticationSuccess/'  + CAS + 'attributes'  , namespaces=NSMAP)
 
-        if attr.find(CAS + 'is_superuser', NSMAP) is not None:
-            user.is_superuser = attr.find(CAS + 'is_superuser', NSMAP).text.upper() == 'TRUE'
+    if attr is not None:
 
-        if attr.find(CAS + 'is_active', NSMAP) is not None:
-            user.is_active = attr.find(CAS + 'is_active', NSMAP).text.upper() == 'TRUE'
+        staff_flag = attr.find(CAS + 'is_staff', NSMAP)
+        if staff_flag is not None:
+            user.is_staff = (staff_flag.text or '').upper() == 'TRUE'
+
+        superuser_flag = attr.find(CAS + 'is_superuser', NSMAP)
+        if superuser_flag is not None:
+            user.is_superuser = (superuser_flag.text or '').upper() == 'TRUE'
+
+        active_flag = attr.find(CAS + 'is_active', NSMAP)
+        if active_flag is not None:
+            user.is_active = (active_flag.text or '').upper() == 'TRUE'
 
         # Limiting by maximum lengths.
         # Max length of firstname/lastname is 30.
         # Max length of a email is 75.
-        if attr.find(CAS + 'givenName', NSMAP) is not None:
-            user.first_name = attr.find(CAS + 'givenName', NSMAP).text[0:30]
 
-        if attr.find(CAS + 'sn', NSMAP) is not None:
-            user.last_name = attr.find(CAS + 'sn', NSMAP).text[0:30]
+        first_name = attr.find(CAS + 'givenName', NSMAP)
+        if first_name is not None:
+            user.first_name = (first_name.text or '')[0:30]
 
-        if attr.find(CAS + 'email', NSMAP) is not None:
-            user.email = attr.find(CAS + 'email', NSMAP).text[0:75]
-        
+        last_name = attr.find(CAS + 'sn', NSMAP)
+        if last_name is not None:
+            user.last_name = (last_name.text or '')[0:30]
+
+        email = attr.find(CAS + 'email', NSMAP)
+        if email is not None:
+            user.email = (email.text or '')[0:75]
+
         # Here we handle things that go into UserProfile instead.
-        
-        # This is a dirty hack and you shouldn't do that. 
+
+        # This is a dirty hack and you shouldn't do that.
         # However, I don't think it's going to work when imported outside of the function body.
-        
+
         from student.models import UserProfile
-        
-        try:
-            user_profile = UserProfile.objects.get(user=user)
-        except UserProfile.DoesNotExist:
-            user.save()
-            user_profile = UserProfile(user=user)
-            
+
+        user_profile, created = UserProfile.objects.get_or_create(user=user)
+        # We don't do that on old edX because it's so bloody fragile.
+        # On the new edX, logging in through CAS will also mean an unusable
+        # password inside.
+        user.set_unusable_password()
+
         # There should be more variables, but let's settle on the actual model first.
-        if attr.find(CAS + 'fullName', NSMAP) is not None:
-            user_profile.name = attr.find(CAS + 'fullName', NSMAP).text
-    
+        full_name = attr.find(CAS + 'fullName', NSMAP)
+        if full_name is not None:
+            user_profile.name = full_name.text or ''
+
         # Profile is always getting saved, just like the user,
         # but the user is getting saved by django_cas.
+        if created:
+            user.save()
         user_profile.save()
 
         # Now the really fun bit. Signing the user up for courses given.
-        
+
         coursetag = attr.find(CAS + 'courses', NSMAP)
         if coursetag is not None:
             try:
@@ -71,11 +83,11 @@ def populate_user(user, authentication_response):
             for course in courses:
                 if course:
                     # Notice that we don't check if a course by that ID actually exists!
-                    # We don't really have the time for this, 
+                    # We don't really have the time for this,
                     # (I seriously suspect this function is getting called more often than once per login)
                     # and CourseEnrollment objects do no checking of their own.
                     # Being enrolled in a deleted course should not be an issue though...
                     org, course, run = course.split('/')
                     CourseEnrollment.enroll(user,CourseLocator(org=org,course=course,run=run))
-        
+
     pass
